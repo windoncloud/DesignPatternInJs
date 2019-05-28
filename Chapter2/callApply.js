6c5110dfca7674661249db85d33587c7c66710c2
Kingsley <448409934@qq.com>
2019-05-29 07:30:29 +0800

    bind

--- Chapter2/callApply.js
+++ Chapter2/callApply.js
@@ -17,3 +17,24 @@ document.getElementById = function ( func ) {
 var getId = document.getElementById
 var div = getId('as')
 alert(div.id)
+
+Function.prototype.bind = function () {
+    var self = this,
+        context = [].shift.call(arguments),
+        args = [].slice.call(arguments);
+    return function () {
+        return self.apply(context, [].concat(args, [].slice.call(arguments)))
+    }
+}
+
+var obj = {
+    name: 'sven'
+}
+
+var func = function(a,b,c,d){
+    alert(this.name)
+    alert([a,b,c,d])
+}.bind(obj,1,2)
+
+var ccc = func(3,4)
+console.log('ccc', ccc)
