bb90c1f0f421f17c82c945a64c6ed90e525193b4
Kingsley <448409934@qq.com>
2019-06-01 23:59:41 +0800

    Array

--- Chapter2/borrow.js
+++ Chapter2/borrow.js
@@ -13,3 +13,14 @@ B.prototype.getName = function () {
 var b = new B('sven')
 
 console.log(b.getName())
+
+(function () {
+    Array.prototype.push.call(arguments, 3)
+    console.log(arguments)
+})(1, 2)
+
+var a = {}
+Array.prototype.push.call(a, 'first')
+console.log(a.length)
+console.log(a)
+console.log(a[0])
